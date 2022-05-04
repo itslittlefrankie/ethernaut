e2f140155a3c8c8facdc548ff96b950d113125a9
The dragon <mrbeesizzle@gmail.com>
2022-05-04 11:58:56 +0300

    updated the example factory code

--- contracts/contracts/levels/ExampleFactory.sol
+++ contracts/contracts/levels/ExampleFactory.sol
@@ -2,18 +2,24 @@
 pragma solidity ^0.6.0;
 
 import './base/Level.sol';
-import './Dummy.sol';
+import './Example.sol';
 
 contract DummyLevel is Level {
+  uint public insertCoin = 0.001 ether;
 
   function createInstance(address _player) override public payable returns (address) {
     _player;
-    return address(new Dummy());
+    require(msg.value >= insertCoin, "Must send at least 0.001 ETH");
+    return address((new Example){value:msg.value}());
   }
 
   function validateInstance(address payable _instance, address _player) override public returns (bool) {
     _player;
-    Dummy instance = Dummy(_instance);
-    return instance.completed();
+    Example instance = Example(_instance);
+    (bool result,) = address(instance).call{value:0}("");
+    !result;
+    return instance._king() != address(this);
   }
+
+  receive() external payable {}
 }
